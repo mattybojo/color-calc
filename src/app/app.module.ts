8c39f852c9ad9c00a3c3f1368b83699ea5f82935
mattybojo <mattbojorquez@gmail.com>
2022-09-15 15:12:35 -0700

    Changed name of project to color-calculator, integrated firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire/compat';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { environment } from 'src/environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule } from '@angular/forms';
 import { ColorBoxComponent } from './shared/color-box/color-box.component';
 
 @NgModule({
@@ -16,7 +17,8 @@ import { ColorBoxComponent } from './shared/color-box/color-box.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    FormsModule
+    FormsModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
