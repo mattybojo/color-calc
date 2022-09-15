4404d64da416505a8d7a3cf0d46a74d7386b01a4
mattybojo <mattbojorquez@gmail.com>
2022-09-15 13:04:29 -0700

    Implemented color conversions using tinycolor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import * as tinycolor from 'tinycolor2';
+import { Color, HSLColor, HTMLNamedColor, NAMED_COLORS, RGBColor } from './app.beans';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,77 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'color-generator';
+
+  colorInput: Color;
+  color: tinycolor.Instance;
+  complementaryColor: tinycolor.Instance;
+  splitComplementaryColors: tinycolor.Instance[];
+  triadColors: tinycolor.Instance[];
+  tetradColors: tinycolor.Instance[];
+  analogousColors: tinycolor.Instance[];
+  monochromaticColors: tinycolor.Instance[];
+  greyscaleColor: tinycolor.Instance;
+
+  htmlNamedColors = NAMED_COLORS;
+  tc = tinycolor; // Used in template
+
+  constructor() {
+    this.color = tinycolor('#FFF');
+    this.colorInput = new Color(this.color.toHslString(), this.color.toHexString(), this.color.toRgbString());
+    this.complementaryColor = tinycolor(this.color.toHexString()).complement();
+    this.splitComplementaryColors = tinycolor(this.color.toHexString()).splitcomplement();
+    this.triadColors = tinycolor(this.color.toHexString()).triad();
+    this.tetradColors = tinycolor(this.color.toHexString()).tetrad();
+    this.greyscaleColor = tinycolor(this.color.toHexString()).greyscale();
+    this.analogousColors = tinycolor(this.color.toHexString()).analogous();
+    this.monochromaticColors = tinycolor(this.color.toHexString()).monochromatic();
+  }
+
+  onColorChange(type: string, hex?: string): void {
+    const color = this.colorInput;
+    switch (type) {
+      case 'hsl':
+        this.color = tinycolor(color.hsl.toString());
+        this.colorInput.hex = this.color.toHexString();
+        this.colorInput.rgb = new RGBColor(this.color.toRgbString().toLowerCase());
+        break;
+      case 'rgb':
+        this.color = tinycolor(color.rgb.toString());
+        this.colorInput.hex = this.color.toHexString();
+        this.colorInput.hsl = new HSLColor(this.color.toHslString());
+        break;
+      case 'hex':
+        this.color = tinycolor(color.hex);
+        this.colorInput.rgb = new RGBColor(this.color.toRgbString().toLowerCase());
+        this.colorInput.hsl = new HSLColor(this.color.toHslString());
+        break;
+      case 'namedColor':
+        this.color = tinycolor(hex);
+        this.colorInput.rgb = new RGBColor(this.color.toRgbString().toLowerCase());
+        this.colorInput.hsl = new HSLColor(this.color.toHslString());
+        this.colorInput.hex = hex!;
+        break;
+      default:
+        console.error(`Unknown type: ${type}`);
+    }
+    this.updateColors();
+  }
+
+  onFilterColors(event: any): void {
+    if (event.target.value) {
+      this.htmlNamedColors = NAMED_COLORS.filter((color: HTMLNamedColor) => color.name.toLowerCase().includes(event.target.value.toLowerCase()));
+    } else {
+      this.htmlNamedColors = NAMED_COLORS;
+    }
+  }
+
+  updateColors(): void {
+    this.complementaryColor = tinycolor(this.color.toHexString()).complement();
+    this.splitComplementaryColors = tinycolor(this.color.toHexString()).splitcomplement();
+    this.triadColors = tinycolor(this.color.toHexString()).triad();
+    this.tetradColors = tinycolor(this.color.toHexString()).tetrad();
+    this.analogousColors = tinycolor(this.color.toHexString()).analogous();
+    this.monochromaticColors = tinycolor(this.color.toHexString()).monochromatic();
+    this.greyscaleColor = tinycolor(this.color.toHexString()).greyscale();
+  }
 }
---
+++ src/app/shared/color-box/color-box.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import * as tinycolor from 'tinycolor2';
+
+@Component({
+  selector: 'cg-color-box',
+  templateUrl: './color-box.component.html',
+  styleUrls: ['./color-box.component.scss']
+})
+export class ColorBoxComponent implements OnInit, OnChanges {
+
+  @Input() color!: tinycolor.Instance;
+  @Input() showLabels: boolean = true;
+
+  hexString: string = '';
+  rgbaString: string = '';
+  hslaString: string = '';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.setClassVariables();
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.setClassVariables();
+  }
+
+  setClassVariables(): void {
+    this.hexString = this.color.toHexString();
+    this.rgbaString = this.color.toRgbString().toLowerCase();
+    this.hslaString = this.color.toHslString();
+  }
+}
